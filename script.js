ac39a5cffc4c6e19d46349c633449a9470ffdaed
ashir556 <ashirjamil112@gmail.com>
2026-08-01 10:29:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.querySelectorAll("a").forEach(link => {
-    link.addEventListener("click", function(e) {
-        e.preventDefault();
-        alert("Button clicked!");
-    });
-});
+alert("NEW SCRIPT LOADED");
